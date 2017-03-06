3796cea371ea936e03ea9641ea0f270ed2ddce0d
al3Co <aldocontrego@gmail.com>
2017-03-06 15:26:38 +0100

    added Introduction code
    Code for introductory video to Xcode8 and Swift3

---
+++ hola/hola/ViewController.swift
@@ -0,0 +1,26 @@
+//
+//  ViewController.swift
+//  hola
+//
+//  Created by Aldo Contreras on 3/6/17.
+//  Copyright © 2017 Aldo Contreras. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+        print ("Hola mundo")
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
